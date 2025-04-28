659bec913a3a87e2b14f20b4dd854bb304579aeb
paradogs-7 <vnyz2002@gmail.com>
2025-04-28 10:28:24 +0300

    First commit

---
+++ target/debug/deps/bootcamp_task_calculator_rust-453c266ce2ae610e.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-task-calculator-rust/target/debug/deps/libbootcamp_task_calculator_rust-453c266ce2ae610e.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-task-calculator-rust/target/debug/deps/bootcamp_task_calculator_rust-453c266ce2ae610e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bootcamp_task_calculator_rust-c24b1136fe3828a1.d
@@ -0,0 +1,5 @@
+/media/volkan/Kişisel/Github/bootcamp-task-calculator-rust/target/debug/deps/libbootcamp_task_calculator_rust-c24b1136fe3828a1.rmeta: src/main.rs
+
+/media/volkan/Kişisel/Github/bootcamp-task-calculator-rust/target/debug/deps/bootcamp_task_calculator_rust-c24b1136fe3828a1.d: src/main.rs
+
+src/main.rs:
